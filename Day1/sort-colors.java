864480584b2be56464667e99222cd132e42cfed3
bhagwat7540 <bhagwatmiglani@gmail.com>
2024-07-16 00:10:16 +0530

    DAY1

---
+++ Day1/sort-colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i=0, j=0, k=nums.length-1;
+
+        while(j<=k){
+            if(nums[j] == 0){
+                swap(nums, i, j);
+                i++;
+                j++;
+            }
+            else if(nums[j] == 2){
+                swap(nums, j, k);
+                k--;
+            }
+            else j++;
+        }
+    }
+
+    private void swap(int []nums, int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
